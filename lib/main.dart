37bb20868f2a8e581f49265b03615ce1ae5ada67
GimaDev <gima.webdev.demo@gmail.com>
2023-04-11 12:12:17 +0500

    register view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -7,6 +9,9 @@ import 'package:geolocation/geolocation_platform_interface.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,27 +25,85 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late DatabaseReference ref;
+  late TextEditingController _controller;
+  var _nameReady = false;
+  final _geolocationPlugin = Geolocation();
+
   @override
   void initState() {
     super.initState();
+
+    _controller = TextEditingController();
+
+    _controller.addListener(() {
+      final text = _controller.text.toLowerCase();
+
+      if (text.length > 2) {
+        setState(() {
+          _nameReady = true;
+        });
+      } else {
+        setState(() {
+          _nameReady = false;
+        });
+      }
+    });
+
     initFirebase();
+    //initTimer();
   }
 
   Future<void> initFirebase() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    ref = FirebaseDatabase.instance.ref("users");
   }
 
-  String _lat = "Unknown";
-  String _lng = "Unknown";
-  String _distance = "Unknown";
-  String _time = "Unknown";
+  void initTimer() {
+    Timer.periodic(Duration(seconds: 5), periodic);
+  }
 
-  final _geolocationPlugin = Geolocation();
+  Future<void> periodic(timer) async {
+    var location = await getLocation();
+
+    if (location != null) {
+      var distance = double.parse(location['distance'] as String);
+
+      // if (distance > 10) {
+      //   print("little2");
+      // }
+
+      await ref.set({
+        "name": "John",
+        "age": 18,
+        "address": {"line1": "100 Mountain View"}
+      });
+    }
+  }
+
+  Widget registerButton() {
+    if (_nameReady) {
+      return ElevatedButton(
+        onPressed: registerHandler,
+        child: const Text('Регистрация'),
+      );
+    } else {
+      return const ElevatedButton(
+        onPressed: null,
+        child: Text('Регистрация'),
+      );
+    }
+  }
+
+  void registerHandler() {
+
+  }
 
   // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> getLocation() async {
+  Future<LocationType?> getLocation() async {
     LocationType? location;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
@@ -50,17 +113,7 @@ class _MyAppState extends State<MyApp> {
       //location = 'Failed to get location.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _lat = "lat: ${location?['lat']}";
-      _lng = "lng: ${location?['lng']}";
-      _distance = "distance: ${location?['distance']}";
-      _time = "time: ${location?['time']}";
-    });
+    return location;
   }
 
   @override
@@ -68,24 +121,63 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Distance human'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: getLocation,
-                child: const Text('Get Location'),
-              ),
-              Text(_lat),
-              Text(_lng),
-              Text(_distance),
-              Text(_time),
-            ],
-          ),
-        ),
+        body: getBody(),
       ),
+    );
+  }
+
+  Widget getBody() {
+    return Center(
+      child: Column(children: [
+        Expanded(
+          flex: 4,
+          child: Row(),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(children: [
+            Expanded(
+              flex: 1,
+              child: Column(),
+            ),
+            Expanded(
+              flex: 8,
+              child: TextField(
+                  controller: _controller,
+                  style: const TextStyle(fontSize: 17, fontFamily: "Verdana"),
+                  decoration: const InputDecoration(
+                      labelText: 'Имя', border: OutlineInputBorder())),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(),
+            )
+          ]),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(children: [
+            Expanded(
+              flex: 1,
+              child: Column(),
+            ),
+            Expanded(
+              flex: 8,
+              child: registerButton(),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(),
+            ),
+          ]),
+        ),
+        Expanded(
+          flex: 4,
+          child: Row(),
+        )
+      ]),
     );
   }
 }
